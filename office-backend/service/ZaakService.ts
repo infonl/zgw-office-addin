9f0710b10c4b130e7a374505829b4dd69d7713fb
DaxRiool <dax_riool@hotmail.com>
2025-06-03 13:09:55 +0200

    fix: changes for PR comments

--- office-backend/service/ZaakService.ts
+++ office-backend/service/ZaakService.ts
@@ -11,22 +11,10 @@ export class ZaakService {
         if (!this.checkzaakNummer(zaakNummer)) {
             return "Geen valide zaaknummer opgegeven";
         }
-        return this.testCallToOPA(zaakNummer);
+        return zaakNummer;
     }
 
     private checkzaakNummer(zaakNummer: string): boolean {
         return zaakNummer == "0" || zaakNummer == null ? false : true;
-    }
-
-    private testCallToOPA(zaakNummer: string): void {
-        fetch(`http://localhost:8001/zaken/api/v1/zaken/${zaakNummer}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept-Crs": "EPSG:4326",
-          "Content-Crs": "EPSG:4326",
-          Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJvZmZpY2UtYWRkLWluIiwiaWF0IjoxNzQ4ODc2NjM1LCJjbGllbnRfaWQiOiJvZmZpY2UtYWRkLWluIiwidXNlcl9pZCI6Im9mZmljZS1hZGQtaW4iLCJ1c2VyX3JlcHJlc2VudGF0aW9uIjoib2ZmaWNlLWFkZC1pbiJ9.MsHB2TwI_OG4CFYe1p3hRtpEZFFoVYdrgGmxGZu0Y-0`,
-        },
-      })
     }
 }
