4b3ea7956a90ad92aeca9959e521ab7b994ee711
sander boer <mail@sanderboer.nl>
2025-06-06 15:22:49 +0200

    Update office-add-in/src/service/office.service.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- office-add-in/src/service/office.service.ts
+++ office-add-in/src/service/office.service.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 INFO.nl
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 import { type HttpService } from "./http.service";
 
 type State = { file: Office.File; currentSlice: number };
