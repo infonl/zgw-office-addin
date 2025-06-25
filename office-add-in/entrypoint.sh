19d5a8c7dee7dd02a336d20c5b3990b2c792c1d7
Bas de Wit <52208549+bas-info-nl@users.noreply.github.com>
2025-06-25 16:49:20 +0200

    fix(docker): replace api localhost reference in manifest
    Ensure we also replace the `api` references to localhost and connect properly.
    
    Solves: PZ-7400

--- office-add-in/entrypoint.sh
+++ office-add-in/entrypoint.sh
@@ -10,4 +10,4 @@ FRONTEND_URL="${FRONTEND_URL:-https://localhost:3000}"
 echo "Frontend URL is set to ${FRONTEND_URL}. Rewriting manifest"
 
 # Ensure the location is set correctly for the files being served
-find /usr/share/nginx/html -type f -exec sed -i -e "s|https://localhost:3000|$FRONTEND_URL|g" -e "s|https://www.contoso.com|$FRONTEND_URL|g" {} +
+find /usr/share/nginx/html -type f -exec sed -i -e "s|://localhost:3000|$FRONTEND_URL|g" -e "s|https://www.contoso.com|$FRONTEND_URL|g" {} +
