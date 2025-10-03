bf15d7ed02d6a3b7668ac20216ef36eba72d0d37
sander boer <mail@sanderboer.nl>
2025-10-03 16:29:25 +0200

    Update office-add-in/vitest.config.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- office-add-in/vitest.config.ts
+++ office-add-in/vitest.config.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 INFO.nl
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 import { defineConfig, mergeConfig } from 'vitest/config'
 import baseConfig from '../vitest.config'
 
