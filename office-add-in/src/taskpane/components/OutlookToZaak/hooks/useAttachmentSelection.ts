e9bf64a83743a1744a244d6667d738059952e2c3
Stef-te-Winkel <stef@info.nl>
2025-10-29 11:10:48 +0100

    improved little code line

--- office-add-in/src/taskpane/components/OutlookToZaak/hooks/useAttachmentSelection.ts
+++ office-add-in/src/taskpane/components/OutlookToZaak/hooks/useAttachmentSelection.ts
@@ -22,7 +22,7 @@ export function useAttachmentSelection() {
         return;
       }
 
-      const emailAttachments = Array.isArray(item.attachments) ? item.attachments : [];
+      const emailAttachments = item.attachments ?? [];
       const subject: string = item.subject || "";
 
       const emailEntry: AttachmentFile = {
