0aec3b5d4da3d1e273b8092502f6ae9610d807b6
Bas de Wit <bas@info.nl>
2026-01-18 01:59:30 +0100

    fix: format esbuild.config.js

--- office-backend/esbuild.config.js
+++ office-backend/esbuild.config.js
@@ -3,22 +3,24 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
-esbuild.build({
-  entryPoints: ['src/app.ts'],
-  bundle: true,
-  platform: 'node',
-  target: 'node22',
-  format: 'esm',
-  outfile: 'dist/app.js',
-  external: [],
-  sourcemap: !isProd,
-  minify: isProd,
-  treeShaking: true,
-  banner: {
-    js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);"
-  }
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    entryPoints: ["src/app.ts"],
+    bundle: true,
+    platform: "node",
+    target: "node22",
+    format: "esm",
+    outfile: "dist/app.js",
+    external: [],
+    sourcemap: !isProd,
+    minify: isProd,
+    treeShaking: true,
+    banner: {
+      js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);",
+    },
+  })
+  .catch(() => process.exit(1));
